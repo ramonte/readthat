fca8d2673f76e58495a3240fddc72a879c817ee1
ramonte <ramon.artner@gmail.com>
2017-11-14 00:19:39 -0200

    Três páginas e menu criados. Ordenação dos posts maneira

--- readthat/core/urls.py
+++ readthat/core/urls.py
@@ -3,5 +3,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index')
+    url(r'^$', views.index, name='index'),
+    url(r'^new$', views.new, name='new'),
+    url(r'^top$', views.top, name='top'),
+    url(r'^hot$', views.hot, name='hot'),
 ]
--- readthat/core/views.py
+++ readthat/core/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.db.models import Count
+import logging
 
 from django.contrib.auth.models import User
+
+from . import models
+
+logger = logging.getLogger(__name__)
 
 # Create your views here.
 def index(request):
@@ -10,3 +16,24 @@ def index(request):
         'users': users,
     }
     return render(request, 'menu.html', context)
+
+def new(request):
+    forum = models.Forum.objects.order_by('-creation')[:5]
+    context = {
+        'forum': forum,
+    }
+    return render(request, 'new.html', context)
+
+def top(request):
+    forum = models.Forum.objects.order_by('-upvotes')[:5]
+    context = {
+        'forum': forum,
+    }
+    return render(request, 'top.html', context)
+
+def hot(request):
+    forum = models.Forum.objects.annotate(num_comments=Count('comentary__forum')).order_by('-num_comments')[:5]
+    context = {
+        'forum': forum,
+    }
+    return render(request, 'hot.html', context)
